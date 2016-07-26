286dd5f1cbc1c77f0d8ec73795757cedef4c3f70
Nick Brown <nicholas.g.brown@gmail.com>
2016-07-26 17:08:50 -0400

    Worked on if-else statements in Javascript for section 3.

---
+++ node-basics/if.js
@@ -0,0 +1,9 @@
+var friend = 'Joe';
+
+if (friend === 'Kenny' || friend === 'Brandon' || friend === 'Makeba') {
+  console.log('It\'s good to see you ' + friend);
+} else if (friend === 'Cortez' || friend === 'Aaron' ) {
+  console.log('Hey');
+} else {
+  console.log('Hey stranger');
+}
