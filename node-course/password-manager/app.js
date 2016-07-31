c871efd8af6eeca119df458a94428d25281c5e7d
Nick Brown <nicholas.g.brown@gmail.com>
2016-07-31 15:32:20 -0400

    Completed the first section of the password manager project. I added createAccount and getAccount functions to app.js and added some code to test them out.

--- node-course/password-manager/app.js
+++ node-course/password-manager/app.js
@@ -3,3 +3,60 @@ console.log('starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
+// account.name Facebook
+// account.username User12!
+// account.password Password123!
+
+function createAccount (account) {
+  var accounts = storage.getItemSync('accounts');
+
+  if (typeof accounts === "undefined") {
+    accounts = [];
+  }
+
+  accounts.push(account);
+
+  storage.setItemSync('accounts', accounts);
+
+  return account;
+}
+
+function getAccount(accountName) {
+
+  var accounts = storage.getItemSync('accounts');
+  var matchingAccount;
+
+  accounts.forEach(function (account) {
+    if (account.name === accountName) {
+      matchingAccount = account;
+    }
+  });
+
+  return matchingAccount;
+}
+
+createAccount({
+  name: "Facebook",
+  username: "facebook_ninja",
+  password: "password123!"
+});
+
+var myAccount = getAccount('Facebook');
+console.log(myAccount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
